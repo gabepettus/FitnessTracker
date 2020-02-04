492277d038cf8b03fb48b48c7410b2d4dee4c9ea
gdpettus <gabe.pettus@gmail.com>
2020-02-03 17:45:30 -1000

    mostly working, need to figuer out the put additional exercieses bit

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -20,7 +20,20 @@ router.post("/api/workouts", (req,res) => {
   });
 });
 
-router.put("/api/workouts/:id", (req,res) => {
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+        Workout.findByIdAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+            { new: true, runValidators: true }
+        )
+            .then(data => res.json(data))
+            .catch(err => {
+                console.log("err", err)
+                res.json(err)
+            })
+    });
+
+router.put("/api/turd/:id", (req,res) => {
   console.log("XXXX put api/workouts XXXX");
   console.log("id",req.params.id);
   console.log("req.body",req.body);
